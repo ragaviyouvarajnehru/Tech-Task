2f3f7da44656627cf385b5d2a70869704fa85754
ragaviyouvarajnehru <113235069+ragaviyouvarajnehru@users.noreply.github.com>
2023-03-29 08:52:05 +0530

    Create oe.java

---
+++ oe.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+class oe
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+System.out.println("Enter the odd or even");
+int a=s.nextInt();
+if(a%2==0)
+{
+System.out.println(a+" is a even");
+}
+else
+{
+System.out.println(a+" is a odd");
+}
+}
+}
